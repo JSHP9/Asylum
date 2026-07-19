6cc2a61899b4df0bf4d59e230c2738dc6fd926f1
JSHP9 <sja179223@gmail.com>
2026-07-19 21:12:43 +0900

    feat: implement enemy sight detection system
    - EnemySight 스크립트 작성
    - CanSeePlayer() 플레이어 감지 함수 구현
    - 거리 기반 감지 로직 추가
    - 시야각 기반 감지 로직 추가
    - Raycast로 벽 판정 구현
    - Unity의 Inspector Attribute을 이용해 인스펙터 창에서 범위 제한 둠
    
    EnmySight는 플렝이어 감지하는것만 넣어둠(SRP)
    
    거리->시야각->Raycast순서로 검사
    
    Raycast로 벽 판정을 구현했는데 문은 Interactable이라, 어떻게 문을 벽으로
    판정하게 할지 고민중.
    이후 AI Chase 제작 예정

---
+++ Assets/Scripts/EnemySight.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    [Header("Sight Settings")]
+    public Transform player; // 플레이어 타겟
+    public float viewRadius = 10f; // 귀신이 볼 수 있는 최대 거리
+    // [Range(0, 360)]은 Unity의 Attribute(특성)이라함, nspector에서 값을 입력할 때 슬라이더를 만들어줌
+    // 이렇게 선언하면 인스펙터에서는 강제력이 생김, 코드는 아님
+    [Range(0, 360)] public float viewAngle = 90f; // 귀신의 시야각, 전방 90도면 좌우 45도씩
+
+    public LayerMask obstacleMask; // 벽, 문 등 시야를 가리는 장애물 레이어
+
+    public bool CanSeePlayer()
+    {
+        // 플레이어와의 거리 잼
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer > viewRadius)
+        {
+            Debug.Log("멀리있어서 안보임");
+            return false; // 사야보다 멀리 있음
+        }
+
+        // 플레이어가 귀신의 시야각 안에 있는지 노말벡터로 각도 재서 구함
+        Vector3 dirToPlayer = (player.position - transform.position).normalized; // 타겟을 향하는 방향
+
+        float angleToPlayer = Vector3.Angle(transform.forward, dirToPlayer);
+
+        // 시야각(viewAngle)이 왼쪽 45, 오른쪽 45해서 90인거라 45보다 크면 시야 밖에 있는거임
+        if (angleToPlayer > viewAngle / 2f)
+        {
+            Debug.Log("시야 밖이라 안보임");
+            return false;
+        }
+
+        // 사이에 벽 있는지 확인
+        // ai위치, 플레이어 각도, 플레이어 거리, 벽 레이어
+        if (Physics.Raycast(transform.position, dirToPlayer, distanceToPlayer, obstacleMask))
+        {
+            Debug.Log("벽에 막혀서 안보임");
+            return false; // 안보임
+        }
+
+        // 보임
+        return true;
+    }
+}
