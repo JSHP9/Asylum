09dfd94141fb5d4b66c33d2039b05049e3c4492c
JSHP9 <sja179223@gmail.com>
2026-07-21 22:32:41 +0900

    feat: implement enemy chase and patrol behavior
    - EnemyAI 스크립트 작성
    - EnemySight와 연동하여 플레이어 추적 구현
    - NavMeshAgent를 이용한 이동 처리
    - 플레이어를 발견하지 못하면 랜덤 배회(Patrol) 구현
    - NavMesh.SamplePosition을 이용해 이동 가능한 위치만 목적지로 설정
    
    EnemyAI는 이동과 상태를 담당하고,
    플레이어 감지는 EnemySight가 담당하도록 역할을 분리하였다. (SRP)
    
    플레이어를 감지하면 NavMeshAgent의 목적지를
    플레이어 위치로 설정하여 추적하도록 구현하였다.
    
    플레이어를 감지하지 못하면
    Random.insideUnitSphere를 이용해 AI 주변의 랜덤 위치를 생성하고,
    NavMesh.SamplePosition으로 유효한 NavMesh 위치를 찾아 배회하도록 구현함.
    
    TODO
    다음 단계에서는 여유 있으면 배회 후 2초정도 가만히 있다가 다시 배회하는
    기능 구현할거고(느낌상 update() 내부에 Time.deltaTime쓰면 될거같음),
    자리에 앉기 기능과 숨기 기능 구현 예정.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,16 +4,36 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private EnemySight sight;
     [SerializeField] private Transform target;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        sight = GetComponent<EnemySight>();
     }
     private void Update()
     {
         if (target == null)
             return;
-        // 매 프레임마다 agent에게 target의 위치로 가라고 목표지점 설정 명령 내림
-        agent.SetDestination(target.position);
+        if (sight.CanSeePlayer())
+        {
+            // 매 프레임마다 agent에게 target의 위치로 가라고 목표지점 설정 명령 내림
+            agent.SetDestination(target.position);
+        }
+        else
+        {
+            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance) // agent의 경로를 계산하지 않고 있고 && 목적지까지 남은 거리가 randomPosition 이하일 때, stoppingDistance는 지정 거리 안에만 들어오면 도착한걸로간주함.
+            { // 랜덤 위치 배회
+
+                NavMeshHit hit; // 랜덤 위치 저장 변수
+                Vector3 randomPosition = transform.position + Random.insideUnitSphere * Random.Range(5, 10); // 랜덤 거리 저장
+                randomPosition.y = transform.position.y; // Random.insideUnitSphere가 x,y,z 전부 반환해줘서(반지름 1인 구 안의 랜덤한 점을 뽑는거라서 그럼) 높이는 현재 높이로 맞춰줌
+                                                          // AI주변 랜덤 위치 생성NavMesh.SamplePosition(기준 좌표, 찾은 NavMesh 위치 저장 변수, 제한 반경, 어떤 NavMesh 영역 쓸지);
+                if (NavMesh.SamplePosition(randomPosition, out hit, 10, NavMesh.AllAreas))
+                {
+                    agent.SetDestination(hit.position); // 거기로 이동
+                }
+            }
+        }
     }
 }
